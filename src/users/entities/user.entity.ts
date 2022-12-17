f88375556d655ff241046a1ae5c835a96708c44a
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2022-12-17 17:18:52 -0400

    feat: config db

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: 'ACTIVATE' })
+  status: 'ACTIVATE' | 'INACTIVATE';
+}
